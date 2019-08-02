0e08462ef3d452bcc16fa7abcf71e6b820c18155
Karen <karenrgo@gmail.com>
2019-08-01 17:49:00 -0700

    removed extra slash

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-BASE_URL = 'https://hidden-headland-56055.herokuapp.com/'
+BASE_URL = 'https://hidden-headland-56055.herokuapp.com'
 USERS_URL = `${BASE_URL}/users`
 CAPSULES_URL = `${BASE_URL}/capsules`
 ARTICLES_URL = `${BASE_URL}/articles`
